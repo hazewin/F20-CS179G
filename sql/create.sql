9a267132fe9ecf75e2687e31c8c3c90a782526be
Hazel Nguyen <hnguy154@ucr.edu>
2020-11-11 16:24:07 -0800

    update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,220 +1,94 @@
-DROP TABLE IF EXISTS Customer CASCADE;--OK
-DROP TABLE IF EXISTS Flight CASCADE;--OK
-DROP TABLE IF EXISTS Pilot CASCADE;--OK
-DROP TABLE IF EXISTS Plane CASCADE;--OK
-DROP TABLE IF EXISTS Technician CASCADE;--OK
+DROP TABLE User CASCADE;
+DROP TABLE Post CASCADE;
+DROP TABLE UserProfile CASCADE;
 
-DROP TABLE IF EXISTS Reservation CASCADE;--OK
-DROP TABLE IF EXISTS FlightInfo CASCADE;--OK
-DROP TABLE IF EXISTS Repairs CASCADE;--OK
-DROP TABLE IF EXISTS Schedule CASCADE;--OK
+-------------
+--SEQUENCES--
+-------------
+
+CREATE SEQUENCE userID START WITH 11;
+CREATE SEQUENCE post_id START WITH 11;
+CREATE SEQUENCE profile_id START WITH 11;
 
 -------------
 ---DOMAINS---
 -------------
-CREATE DOMAIN us_postal_code AS TEXT CHECK(VALUE ~ '^\d{5}$' OR VALUE ~ '^\d{5}-\d{4}$');
-CREATE DOMAIN _STATUS CHAR(1) CHECK (value IN ( 'W' , 'C', 'R' ) );
-CREATE DOMAIN _GENDER CHAR(1) CHECK (value IN ( 'F' , 'M' ) );
-CREATE DOMAIN _CODE CHAR(2) CHECK (value IN ( 'MJ' , 'MN', 'SV' ) ); --Major, Minimum, Service
-CREATE DOMAIN _PINTEGER AS int4 CHECK(VALUE > 0);
-CREATE DOMAIN _PZEROINTEGER AS int4 CHECK(VALUE >= 0);
-CREATE DOMAIN _YEAR_1970 AS int4 CHECK(VALUE >= 0);
-CREATE DOMAIN _SEATS AS int4 CHECK(VALUE > 0 AND VALUE < 500);--Plane Seats
+
+CREATE DOMAIN _STATUS CHAR(5) CHECK (value IN ( 'TRUE' , 'FALSE') );
 
 ------------
 ---TABLES---
 ------------
-CREATE TABLE Customer
-(
-	id INTEGER NOT NULL,
-	fname CHAR(24) NOT NULL,
-	lname CHAR(24) NOT NULL,
-	gtype _GENDER NOT NULL,
-	dob DATE NOT NULL,
-	address CHAR(256),
-	phone CHAR(10),
-	zipcode char(10),
-	PRIMARY KEY (id)
-);
 
-CREATE TABLE Pilot
-(
-	id INTEGER NOT NULL,
-	fullname CHAR(128),
-	nationality CHAR(24),
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE Flight
-(
-	fnum INTEGER NOT NULL,
-	cost _PINTEGER NOT NULL,
-	num_sold _PZEROINTEGER NOT NULL,
-	num_stops _PZEROINTEGER NOT NULL,
-	actual_departure_date DATE NOT NULL,
-	actual_arrival_date DATE NOT NULL,
-	arrival_airport CHAR(5) NOT NULL,-- AIRPORT CODE --
-	departure_airport CHAR(5) NOT NULL,-- AIRPORT CODE --
-	PRIMARY KEY (fnum)
-);
-
-CREATE TABLE Plane
-(
-	id INTEGER NOT NULL,
-	make CHAR(32) NOT NULL,
-	model CHAR(64) NOT NULL,
-	age _YEAR_1970 NOT NULL,
-	seats _SEATS NOT NULL,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE Technician
-(
-	id INTEGER NOT NULL,
-	full_name CHAR(128) NOT NULL,
-	PRIMARY KEY (id)
+CREATE TABLE User(
+userID INTEGER NOT NULL,
+fullname VARCHAR(128) NOT NULL,
+username VARCHAR(64) NOT NULL,
+email VARCHAR(64) NOT NULL,
+user_password CHAR(64) NOT NULL,
+PRIMARY KEY(username)
 );
 
 ---------------
 ---RELATIONS---
 ---------------
 
-CREATE TABLE Reservation
-(
-	rnum INTEGER NOT NULL,
-	cid INTEGER NOT NULL,
-	fid INTEGER NOT NULL,
-	status _STATUS,
-	PRIMARY KEY (rnum),
-	FOREIGN KEY (cid) REFERENCES Customer(id),
-	FOREIGN KEY (fid) REFERENCES Flight(fnum)
+CREATE TABLE Post(
+post_id INTEGER NOT NULL,
+username_id VARCHAR(64) NOT NULL,
+likes INTEGER NOT NULL,
+date_posted DATE NOT NULL,
+num_comments INTEGER NOT NULL,
+tags VARCHAR(64) NOT NULL,
+photo_url VARCHAR(128) NOT NULL,
+PRIMARY KEY(photo_id),
+FOREIGN KEY(username_id) REFERENCES User(username)
 );
 
-CREATE TABLE FlightInfo
-(
-	fiid INTEGER NOT NULL,
-	flight_id INTEGER NOT NULL,
-	pilot_id INTEGER NOT NULL,
-	plane_id INTEGER NOT NULL,
-	PRIMARY KEY (fiid),
-	FOREIGN KEY (flight_id) REFERENCES Flight(fnum),
-	FOREIGN KEY (pilot_id) REFERENCES Pilot(id),
-	FOREIGN KEY (plane_id) REFERENCES Plane(id)
+CREATE TABLE UserProfile(
+profile_id INTEGER NOT NULL,
+username_id VARCHAR(64) NOT NULL,
+num_posts INTEGER NOT NULL,
+followers INTEGER NOT NULL,
+followings INTEGER NOT NULL,
+follow_status _STATUS,
+FOREIGN KEY(username_id) REFERENCES User(username)
 );
 
-CREATE TABLE Repairs
-(
-	rid INTEGER NOT NULL,
-	repair_date DATE NOT NULL,
-	repair_code _CODE,
-	pilot_id INTEGER NOT NULL,
-	plane_id INTEGER NOT NULL,
-	technician_id INTEGER NOT NULL,
-	PRIMARY KEY (rid),
-	FOREIGN KEY (pilot_id) REFERENCES Pilot(id),
-	FOREIGN KEY (plane_id) REFERENCES Plane(id),
-	FOREIGN KEY (technician_id) REFERENCES Technician(id)
-);
-
-CREATE TABLE Schedule
-(
-	id INTEGER NOT NULL,
-	flightNum INTEGER NOT NULL,
-	departure_time DATE NOT NULL,
-	arrival_time DATE NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (flightNum) REFERENCES Flight(fnum)
-);
 
 ----------------------------
 -- INSERT DATA STATEMENTS --
 ----------------------------
 
-COPY Customer (
-	id,
-	fname,
-	lname,
-	gtype,
-	dob,
-	address,
-	phone,
-	zipcode
+COPY User (
+	userID,
+    fullname,
+    username,
+    email,
+    user_password
 )
-FROM 'customer.csv'
+FROM 'users.csv'
 WITH DELIMITER ',';
 
-COPY Pilot (
-	id,
-	fullname,
-	nationality
+COPY Post (
+	post_id,
+	username_id,
+    likes,
+    date_posted,
+    num_comments,
+    tags,
+    photo_url
 )
-FROM 'pilots.csv'
+FROM 'posts.csv'
 WITH DELIMITER ',';
 
-COPY Plane (
-	id,
-	make,
-	model,
-	age,
-	seats
+COPY UserProfile (
+	profile_id,
+	username_id,
+    num_posts,
+    followers,
+    followings,
+    follow_status
 )
-FROM 'planes.csv'
-WITH DELIMITER ',';
-
-COPY Technician (
-	id,
-	full_name
-)
-FROM 'technician.csv'
-WITH DELIMITER ',';
-
-COPY Flight (
-	fnum,
-	cost,
-	num_sold,
-	num_stops,
-	actual_departure_date,
-	actual_arrival_date,
-	arrival_airport,
-	departure_airport
-)
-FROM 'flights.csv'
-WITH DELIMITER ',';
-
-COPY Reservation (
-	rnum,
-	cid,
-	fid,
-	status
-)
-FROM 'reservation.csv'
-WITH DELIMITER ',';
-
-COPY FlightInfo (
-	fiid,
-	flight_id,
-	pilot_id,
-	plane_id
-)
-FROM 'flightinfo.csv'
-WITH DELIMITER ',';
-
-COPY Repairs (
-	rid,
-	repair_date,
-	repair_code,
-	pilot_id,
-	plane_id,
-	technician_id
-)
-FROM 'repairs.csv'
-WITH DELIMITER ',';
-
-COPY Schedule (
-	id,
-	flightNum,
-	departure_time,
-	arrival_time
-)
-FROM 'schedule.csv'
+FROM 'profiles.csv'
 WITH DELIMITER ',';
