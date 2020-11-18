f60d6bb796f568a32bdc2c252949dc61e51ee5a7
tsandytsan <tsandytsan@gmail.com>
2020-11-18 15:06:45 -0800

    pushing hazel's changes onto my branch i hope

--- postgresql/createPostgreDB.sh
+++ postgresql/createPostgreDB.sh
@@ -16,16 +16,9 @@ pg_ctl status
 
 echo "Copying csv files ... "
 sleep 1
-<<<<<<< HEAD:postgresql/createPostgreDB.sh
-cp ../data/*.csv /tmp/$USER/myDB/data/.
 
-echo "Initializing tables .. "
-sleep 1
-psql -h localhost -p $PGPORT $USER"_DB" < ../sql/create.sql
-=======
 cp data/*.csv /tmp/$(logname)/mydb/data/
 
 echo "Initializing tables .. "
 sleep 1
 psql -h localhost $dbname < sql/create.sql
->>>>>>> d021635979fefa4016db6d18cd6288d07046ad16:createPostgreDB.sh
